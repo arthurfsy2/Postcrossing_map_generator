be76bff853f767581de582e81c2f056cb4f62bdb
arthurfsy2 <fsyflh@gmail.com>
2023-10-27 16:16:15 +0800

    modify

--- postcrossing.py
+++ postcrossing.py
@@ -16,7 +16,6 @@ with open("config.json", "r") as file:
 account = data["account"]
 nickName = data["nickName"]
 Cookie = data["Cookie"]
-isPrivate = data["isPrivate"]
 picDriverPath = data["picDriverPath"]
 
 # 获取当前日期
@@ -25,12 +24,11 @@ current_date = datetime.now().date()
 # 将日期格式化为指定格式
 date = current_date.strftime("%Y-%m-%d")
 
-url = f"https://www.postcrossing.com/user/{account}/gallery"  # 设置该账号的展示墙
+userUrl = f"https://www.postcrossing.com/user/{account}"  
+galleryUrl = f"{userUrl}/gallery"  # 设置该账号的展示墙
+dataUrl = f"{userUrl}/data/sent"  
 
-if isPrivate =="Y":
-    types = ['sent', 'received', 'favourites', 'popular']  # 设置个人账号的取值类别
-else:
-    types = ['sent', 'received']
+
 
 headers = {
     'authority': 'www.postcrossing.com',
@@ -40,15 +38,35 @@ headers = {
 
 #获取账号状态
 def getAccountStat():
-    response = requests.get(url,headers=headers)
-    response_status = response.status_code
-    content = response.text.replace('"//', '"https://')
-    return response_status,content
+    galleryResponse = requests.get(galleryUrl,headers=headers)
+    galleryStatus = galleryResponse.status_code
+    galleryContent = galleryResponse.text.replace('"//', '"https://')
+
+    dataResponse = requests.get(dataUrl,headers=headers)
+    dataContent = dataResponse.text
+    if "Log in to see this content" in dataContent:
+        cookieStat = 404
+    else:
+        cookieStat = 200
+    if galleryStatus == 200 and cookieStat == 200:
+        totalStat ="getPrivate"
+        types = ['sent', 'received', 'favourites', 'popular']
+        print(f"{account}的Cookies有效，可访问个人账号内容")
+    elif galleryStatus == 200 and cookieStat == 404:
+        totalStat ="getPublic"
+        types = ['sent', 'received'] 
+        print(f"{account}的Cookies无效，只能访问gallery内容")
+    elif galleryStatus != 200:
+        totalStat ="unAccessible"
+        print(f"用户:{account}已注销/设置为非公开，无法获取！\n")
+        sys.exit()
+
+    return totalStat,galleryContent,types
 
 
-stat,content_raw = getAccountStat()
+stat,content_raw,types = getAccountStat()
 
-#获取account的sent, received, favourites, popular对应的数量
+#获取account的types对应的数量
 def get_message_counts(content):
     # 定义正则表达式模式
     counts = ()
@@ -70,36 +88,14 @@ counts = get_message_counts(content_raw)
 print(f"计数：{counts}")
 
 
-def getPostCardPic(type,response_status, counts):
-    print(f"\n{account}展示墙链接：{url}")      
-    sentNum = counts[0]
-    sentPageNum = counts[1]
-    receivedNum = counts[2]
-    receivedPageNum = counts[3]
-    if isPrivate =="Y":
-        response = requests.get(url,headers=headers)
-        favouritesNum = counts[4]
-        favouritesPageNum = counts[5]
-        popularNum = counts[6]
-        popularPageNum = counts[7]
+def createMD(type, counts):
+    print(f"\n{account}展示墙链接：{galleryUrl}/{type}")      
+    sentNum, sentPageNum, receivedNum, receivedPageNum = counts[:4]
+    if stat == "getPrivate":
+        favouritesNum, favouritesPageNum, popularNum, popularPageNum = counts[4:8]
     else:
-        response = requests.get(url)
-        favouritesNum = 0
-        favouritesPageNum = 0
-        popularNum = 0
-        popularPageNum = 0
-
-    response_status = response.status_code
-    
-    
-    
-    content = response.text.replace('"//', '"https://"')
-    #filename = f"./output/{account}_{type}({i}).html"
-    """ with open(filename, "w", encoding="utf-8") as file:
-                content_i = response.text.replace('"//', '"https://')
-                file.write(content_i)
-                print(f"\n{type}({i})数据保存成功：{filename}") """
-    
+        favouritesNum = favouritesPageNum = popularNum = popularPageNum = 0
+     
     data = {
     "received": ("来自", receivedPageNum, receivedNum, f"明信片展示墙（收到：{receivedNum}）"),
     "sent": ("寄往", sentPageNum, sentNum, f"明信片展示墙（寄出：{sentNum}）"),
@@ -107,11 +103,14 @@ def getPostCardPic(type,response_status, counts):
     "popular": ("", popularPageNum, popularNum, f"明信片展示墙（我收到的赞：{popularNum}）")
             }
     value = data.get(type)
-    if value:
-        # 获取元组中的变量
-        from_or_to, pageNum, Num, title = value
+    from_or_to, pageNum, Num, title = value
     print(f"{account}'{type}展示墙数量:{Num}\n{account}'{type}展示墙页数:{pageNum}\n")
-    galleryInfo(response_status, pageNum ,from_or_to, title)
+    MDcontent_all = getMDcontent(pageNum ,from_or_to)
+    filename_md = f"gallery/{type}.md"
+    with open(filename_md, "w", encoding="utf-8") as file:
+            content = f'---\ntitle: {title}\nicon: address-card\ndate: {date}\ncategory:\n  - {nickName}\ntag:\n  - postcrossing\n---\n\n{MDcontent_all}'
+            file.write(content)
+            print(f"{type}_展示墙数据转换为md格式成功：{filename_md}\n")
 
 def getCountryName(flag):
     # 读取contryName.json文件
@@ -121,84 +120,69 @@ def getCountryName(flag):
     value = data.get(flag)
     return value
 
-def galleryInfo(response_status, pageNum ,from_or_to, title):
+def getMDcontent(pageNum ,from_or_to):
     i = 1
-    picurl_all = ""
+    MDcontent_all = ""
     while i <= pageNum:
-        if response_status == 200:
-            all_url = f"{url}/{type}/{i}"
-            print(f"正在获取/{account}/gallery/{type}/{i}的数据")
-            response = requests.get(all_url,headers=headers)
-            filename_md = f"gallery/{type}.md"
-            # filename = f"./output/{type}({i}).html"
-            # with open(filename, "w", encoding="utf-8") as file:
-            #     content_i = response.text.replace('"//', '"https://')
-            #     file.write(content_i)
-                #print(f"\n{type}({i})数据保存成功：{filename}") 
-            content_i = response.text.replace('"//', '"https://')
-            soup = BeautifulSoup(content_i, "html.parser")
-            lis = soup.find_all("li")
-            picurl_page = ""
-            
-            for li in lis:
-                figure = li.find("figure")
-                figcaption = li.find("figcaption")
-                if figure:
-                    href = figure.find("a")["href"]
-                    #print(f"href:{href}")
-                    postcardID = figcaption.find("a").text
-                    
-                    picFileName = re.search(r"/([^/]+)$", href).group(1)
-                    #print(f"picFileName:{picFileName}")                   
-                    picDownloadPath = f"gallery/picture/{picFileName}"  # 替换为你要保存的文件路径和文件名
-                    if os.path.exists(picDownloadPath):
-                        #print(f"已存在{picDownloadPath}")
-                        pass
-                    else:
-                        picDownloadurl = f"https://s3.amazonaws.com/static2.postcrossing.com/postcard/medium/{picFileName}"
-                        print(f"picDownloadurl:{picDownloadurl}")
-                        response = requests.get(picDownloadurl)
-                        print(f"正在下载{picDownloadPath}的图片")
-                        with open(picDownloadPath, "wb") as file:
-                            file.write(response.content)
-                    baseUrl = "https://www.postcrossing.com/"
-                    if type == "popular":
-                        favoritesNum = figcaption.find("div").text
-                        num = re.search(r'\d+', favoritesNum).group()
-                        picurl = f"## [{postcardID}]({baseUrl}postcards/{postcardID})\n>点赞人数：{num}\n\n![]({href}) \n "
-                    else:
-                        user = figcaption.find("div").find("a").text
-                        userInfo = f"[{user}]({baseUrl}user/{user})"
-                        if not user:
-                            userInfo = "***该用户已关闭***"
-                        flag = re.search(r'href="/country/(.*?)"', str(figcaption)).group(1)
-                        contryName = getCountryName(flag)
-                        picurl = f"## [{postcardID}]({baseUrl}postcards/{postcardID}) \n >{from_or_to} {userInfo} {contryName}\n\n![]({picDriverPath}/{picFileName})\n\n"
-                    picurl_page += picurl                
-            picurl_all += picurl_page
-            
-            
-            with open(filename_md, "w", encoding="utf-8") as file:
-                
-                content = f'---\ntitle: {title}\nicon: address-card\ndate: {date}\ncategory:\n  - {nickName}\ntag:\n  - postcrossing\n---\n\n{picurl_all}'
-                file.write(content)
-                print(f"{type}_展示墙数据转换为md格式成功：{filename_md}\n")
-            i += 1
-
-        else:
-            print(f"用户:{account}已注销/设置为非公开，无法获取！\n")
-            return None
-
-
-if stat == 200:
-    print(f"用户:{account}的数据为公开，可获取！\n")
-    for type in types:
-        getPostCardPic(type, stat, counts) 
+        all_url = f"{galleryUrl}/{type}/{i}"
+        print(f"正在获取/{account}/gallery/{type}/{i}的数据")
+        response = requests.get(all_url,headers=headers)
         
-else:
-    print(f"用户:{account}已注销/设置为非公开，无法获取！\n")
-    sys.exit()
+        content_i = response.text.replace('"//', '"https://')
+        soup = BeautifulSoup(content_i, "html.parser")
+        lis = soup.find_all("li")
+        picurl_page = ""
+        
+        # 获取每个postcard信息
+        for li in lis:
+            figure = li.find("figure")
+            figcaption = li.find("figcaption")
+            if figure:
+                href = figure.find("a")["href"]
+                #print(f"href:{href}")
+                postcardID = figcaption.find("a").text                   
+                picFileName = re.search(r"/([^/]+)$", href).group(1)
+                #print(f"picFileName:{picFileName}")                   
+                picDownloadPath = f"gallery/picture/{picFileName}"  # 替换为你要保存的文件路径和文件名
+                if os.path.exists(picDownloadPath):
+                    #print(f"已存在{picDownloadPath}")
+                    pass
+                else:
+                    picDownloadurl = f"https://s3.amazonaws.com/static2.postcrossing.com/postcard/medium/{picFileName}"
+                    #print(f"picDownloadurl:{picDownloadurl}")
+                    response = requests.get(picDownloadurl)
+                    print(f"正在下载{picFileName}")
+                    with open(picDownloadPath, "wb") as file:
+                        file.write(response.content)
+                baseUrl = "https://www.postcrossing.com/"
+                if type == "popular":
+                    favoritesNum = figcaption.find("div").text
+                    num = re.search(r'\d+', favoritesNum).group()
+                    picurl = f"## [{postcardID}]({baseUrl}postcards/{postcardID})\n>点赞人数：{num}\n\n![]({href}) \n "
+                else:
+                    user = figcaption.find("div").find("a").text
+                    userInfo = f"[{user}]({baseUrl}user/{user})"
+                    if not user:
+                        userInfo = "***该用户已关闭***"
+                    flag = re.search(r'href="/country/(.*?)"', str(figcaption)).group(1)
+                    contryName = getCountryName(flag)
+                    picurl = f"## [{postcardID}]({baseUrl}postcards/{postcardID}) \n >{from_or_to} {userInfo} {contryName}\n\n![]({picDriverPath}/{picFileName})\n\n"
+                picurl_page += picurl                
+        MDcontent_all += picurl_page
+        i += 1
+        return MDcontent_all
 
-end_time = time.time()
-execution_time = round((end_time - start_time),3)
-print(f"脚本执行时间：{execution_time}秒")
+
+
+# if stat == 200:
+#     print(f"用户:{account}的数据为公开，可获取！\n")
+#     for type in types:
+#         createMD(type, stat, counts) 
+for type in types:
+    createMD(type, counts) 
+        
+
+
+# end_time = time.time()
+# execution_time = round((end_time - start_time),3)
+# print(f"脚本执行时间：{execution_time}秒")
